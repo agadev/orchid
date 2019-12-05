d5dfb05cbcd3d18b8dfb915672fa73127ede8226
atul garg <gargat@gmail.com>
2019-12-05 13:02:42 -0500

    fix(openvpn_hop_page): fix keyboard scroll up error

--- app-shared/lib/pages/circuit/openvpn_hop_page.dart
+++ app-shared/lib/pages/circuit/openvpn_hop_page.dart
@@ -55,127 +55,118 @@ class _OpenVPNHopPageState extends State<OpenVPNHopPage> {
 
   @override
   Widget build(BuildContext context) {
-    return TapClearsFocus(
-      child: TitledPage(
-        title: "OpenVPNHop",
-        lightTheme: false,
-        actions: widget.mode == HopEditorMode.Create
-            ? [widget.buildSaveButton(context)]
-            : [],
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                // Username
-                // pady(16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Username",
-                        style: AppText.textLabelStyle.copyWith(fontSize: 14.0)),
-                    pady(8),
-                    AppTextField(
-                        hintText: "Username",
-                        margin: EdgeInsets.zero,
-                        controller: _userName)
-                  ],
-                ),
-
-                // Password
-                pady(16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Password",
-                        style: AppText.textLabelStyle.copyWith(fontSize: 14.0)),
-                    pady(8),
-                    AppTextField(
-                        hintText: "Password",
-                        margin: EdgeInsets.zero,
-                        controller: _userPassword)
-                  ],
-                ),
-
-                // OPVN Config
-                pady(16),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "Config",
-                      style: AppText.textLabelStyle.copyWith(fontSize: 14.0),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 10.0),
-                    ),
-                    Image.asset(
-                      "assets/images/info_outline.png",
-                      height: 18,
-                      color: AppColors.purple,
-                    ),
-                  ],
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 0, right: 0, top: 8, bottom: 8),
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                      child: SingleChildScrollView(
-                          child: Stack(
-                        children: <Widget>[
-                          TextFormField(
-                            autocorrect: false,
-                            autofocus: false,
-                            keyboardType: TextInputType.multiline,
-                            style: AppText.logStyle
-                                .copyWith(color: AppColors.grey_2),
-                            controller: _ovpnConfig,
-                            maxLines: 99999,
-                            decoration: InputDecoration(
-                              hintText: "Paste your OVPN config file here",
-                              border: InputBorder.none,
-                              labelStyle: AppText.textLabelStyle,
-                            ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
+      body: TapClearsFocus(
+        child: TitledPage(
+          title: "OpenVPNHop",
+          lightTheme: false,
+          actions: widget.mode == HopEditorMode.Create
+              ? [widget.buildSaveButton(context)]
+              : [],
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: SafeArea(
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: <Widget>[
+                  Text("Username",
+                      style: AppText.textLabelStyle.copyWith(fontSize: 14.0)),
+                  pady(8),
+                  AppTextField(
+                    hintText: "Username",
+                    margin: EdgeInsets.zero,
+                    controller: _userName,
+                  ),
+                  pady(16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[],
+                  ),
+                  Text("Password",
+                      style: AppText.textLabelStyle.copyWith(fontSize: 14.0)),
+                  pady(8),
+                  AppTextField(
+                    hintText: "Password",
+                    margin: EdgeInsets.zero,
+                    controller: _userPassword,
+                  ),
+                  pady(16),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        "Config",
+                        style: AppText.textLabelStyle.copyWith(fontSize: 14.0),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 10.0),
+                      ),
+                      Image.asset(
+                        "assets/images/info_outline.png",
+                        height: 18,
+                        color: AppColors.purple,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 150.0,
+                    width: double.infinity,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                    child: SingleChildScrollView(
+                        child: Stack(
+                      children: <Widget>[
+                        TextFormField(
+                          autocorrect: false,
+                          autofocus: false,
+                          keyboardType: TextInputType.multiline,
+                          style: AppText.logStyle
+                              .copyWith(color: AppColors.grey_2),
+                          controller: _ovpnConfig,
+                          maxLines: 99999,
+                          decoration: InputDecoration(
+                            hintText: "Paste your OVPN config file here",
+                            border: InputBorder.none,
+                            labelStyle: AppText.textLabelStyle,
                           ),
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: Icon(
-                              Icons.content_paste,
-                              color: AppColors.green_4,
-                              size: 18.0,
-                            ),
-                          )
-                        ],
-                      )),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        border:
-                            Border.all(width: 2.0, color: AppColors.neutral_5),
+                        ),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Icon(
+                            Icons.content_paste,
+                            color: AppColors.green_4,
+                            size: 18.0,
+                          ),
+                        )
+                      ],
+                    )),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      border:
+                          Border.all(width: 2.0, color: AppColors.neutral_5),
+                    ),
+                  ),
+                  // Instructions
+                  Visibility(
+                    visible: widget.mode == HopEditorMode.Create,
+                    child: Container(
+                      height: 130.0,
+                      width: double.infinity,
+                      child: InstructionsView(
+                        // TODO: This screen is being told it's in landscape mode in the simulator?
+                        //hideInLandscape: false,
+                        title: "Enter your credentials",
+                        body:
+                            "Enter the login information for your VPN provider above. Then paste the contents of your provider’s OpenVPN config file into the field provided. ",
                       ),
                     ),
                   ),
-                ),
-
-                // Instructions
-                Visibility(
-                  visible: widget.mode == HopEditorMode.Create,
-                  child: Expanded(
-                    flex: 1,
-                    child: InstructionsView(
-                      // TODO: This screen is being told it's in landscape mode in the simulator?
-                      //hideInLandscape: false,
-                      title: "Enter your credentials",
-                      body:
-                          "Enter the login information for your VPN provider above. Then paste the contents of your provider’s OpenVPN config file into the field provided. ",
-                    ),
-                  ),
-                ),
-                pady(24)
-              ],
+                  pady(24.0),
+                ],
+              ),
             ),
           ),
         ),
